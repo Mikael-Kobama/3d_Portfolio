ac1d4b7cd1e985a933e0904049bcc52a83e47fbc
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-01-04 21:07:39 -0300

    Add Contact and Navbar update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,51 +45,93 @@ const Contact = () => {
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
-      .then(() => {
-        setLoading(false);
-        alert("Obrigado. Eu irei retornar a mensagem assim que for possível");
+      .then(
+        () => {
+          setLoading(false);
+          alert("Obrigado. Eu irei retornar a mensagem assim que for possível");
 
-        setForm({
-          name: "",
-          email: "",
-          message: "",
-        });
-      },
-      (error) => {
-        setLoading(false);
-        console.log(error);
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
 
-        alert("Ahh, Algo acabou não funcionando. Por favor tente novamente.")
-      }
-    );
+          alert("Ahh, Algo acabou não funcionando. Por favor tente novamente.");
+        }
+      );
   };
 
   return (
-  <div
-  className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
-    <motion.div
-    variants={slideIn("left", "tween", 0.2, 1)}
-    className="flex-[0.75] bg-blue-100 p-8 rounded-2xl">
-      <p className={styles.sectionSubText}>Entre em contato</p>
-      <h3 className={styles.sectionHeadText}>Contato</h3>
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-blue-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Entre em contato</p>
+        <h3 className={styles.sectionHeadText}>Contato</h3>
 
-      <form
-      ref={formRef}
-      onSubmit={handleSubmit}
-      className="mt-12 flex flex-col gap-8">
-        <label className="flex flex-col">
-          <span className="text-white font-medium mb-4">Seu Nome</span>
-          <input type="text"
-          name="name"
-          value={form.value}
-          onChange={handleChange}
-          placeholder="Qual é o seu nome, por favor?"
-          className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium" />
-        </label>
-      </form>
-    </motion.div>
-  </div>
-)
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Seu Nome</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="Qual é o seu nome, por favor?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Seu E-mail</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="Qual é o seu e-mail?"
+              className="bg-tertiary py-4 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Sua Mensagem</span>
+            <textarea
+              name="message"
+              rows={7}
+              value={form.message}
+              onChange={handleChange}
+              placeholder="Qual a sua mensagem?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+          >
+            {loading ? "Enviando..." : "Enviado!"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
 };
 
-export default Contact;
+export default SectionWrapper(Contact, "contact");
