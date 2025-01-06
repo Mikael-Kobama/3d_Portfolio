4530c1328d096e530b3e5ed6930c02dddc1bf3a4
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-01-05 23:32:42 -0300

    update Tech.jsx and Start Hero.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,8 +6,12 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return <div className="flex flex-row flex-wrap justify-center gap-10">
-    Tech
+    {technologies.map((technology) => {
+      <div className="w-28 h-28" key={technology.name}>
+        <BallCanvas icon={technology.icon}/>
+      </div>
+    })}
   </div>;
 };
 
-export default Tech;
+export default SectionWrapper(Tech, "");
